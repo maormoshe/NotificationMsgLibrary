16e771d87d0760b4c4a5248553a780dc7dcc8e0c
mmoshe <mmoshe@outbrain.com>
2019-11-15 12:10:16 +0200

    init repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NgxNotificationMsgService} from '../../projects/ngx-notification-msg/src/lib/ngx-notification-msg.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'NotificationMsgLibrary';
+    header = 'Ops...';
+    msg = 'Anyone with access can view your invited visitors.';
+
+    constructor(private readonly ngxNotificationMsgService: NgxNotificationMsgService) {
+
+    }
+
+    open(status: any): void {
+        this.ngxNotificationMsgService.open({
+            status,
+            header: this.header,
+            msg: this.msg,
+            delay: 300000000
+        });
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {NgxNotificationMsgModule} from '../../projects/ngx-notification-msg/src/lib/ngx-notification-msg.module';
+import {NgxNotificationMsgComponent} from '../../projects/ngx-notification-msg/src/lib/ngx-notification-msg.component';
 
 @NgModule({
+  entryComponents: [
+    NgxNotificationMsgComponent
+  ],
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NgxNotificationMsgModule
   ],
   providers: [],
   bootstrap: [AppComponent]
