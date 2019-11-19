8e5f7981cb033b02b9aa3b0154510d5135a43841
mmoshe <mmoshe@outbrain.com>
2019-11-19 20:31:52 +0200

    updating example app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,32 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NgxNotificationMsgService} from '../../projects/ngx-notification-msg/src/lib/ngx-notification-msg.service';
+import {INgxNotificationMsgConfig} from '../../projects/ngx-notification-msg/src/lib/ngx-notification-msg.component';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-    header = 'Ops...';
-    msg = 'Anyone with access can view your invited visitors.';
+export class AppComponent implements OnInit {
+    inputsConfig: INgxNotificationMsgConfig;
 
     constructor(private readonly ngxNotificationMsgService: NgxNotificationMsgService) {
 
     }
 
+    ngOnInit(): void {
+        this.inputsConfig = {
+            header: 'Ops...',
+            msg: 'Anyone with access can view your invited visitors.',
+            delay: 3000,
+            closeable: true
+        };
+    }
+
     open(status: any): void {
         this.ngxNotificationMsgService.open({
             status,
-            header: this.header,
-            msg: this.msg
+            ...this.inputsConfig
         });
     }
 }
